23e81c0ae0fb9d7288273fd30d530288bbfeec08
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-03-31 10:03:58 +0200

    first commit

---
+++ find_static_casts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# TODO: remove hardcoded paths and make it more flexible and more generally
+# applicable.
+
+find . \( -name "*.cpp" -o -name "*.h" \) \
+         -a -not \( -name "*moc_*" -o -name "*ui_*" -o -name "*qrc_*" \
+                    -o -path "*/Libraries/ICCommon/Qt/*" \
+                    -o -path "*/Libraries/googletest/*" \
+                    -o -path "*/CryptoPP/*" \
+                    -o -path "*/IntelTBB/*" \
+                    -o -path "*/OpenCV/*" \
+                    -o -path "*/MatrixVisionSDK/*" \
+                 \) \
+  -exec grep -Hl 'static_cast' {} \;
