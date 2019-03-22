8b772500fd23d7178b83b0af26b8dfd02f0d6e2e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-03-22 08:25:25 +0100

    Add more screen output to inform user.

--- prepare_vendor_branch.sh
+++ prepare_vendor_branch.sh
@@ -8,10 +8,11 @@
 #   1. You have an extracted tarball/zip of the original source in dir_orig
 #   2. You have a working copy that is an extracted tarball with the
 #      svn source of the previous version checked out on top of it,
-#      in $dir_working
+#      in $dir_working.
 #
-# Example:
+# Example usage:
 #
+#   => run Cygwin as root (TODO: check if this is necessary).
 #   ./prepare_vendor_branch.sh cmake-3.13.4-orig cmake-3.13.4
 
 
@@ -21,15 +22,21 @@ dir_working=$2
 echo "Directory with the original source: $dir_orig"
 echo "Directory with the working copy: $dir_working"
 
-# Svn add any vendor drop items to version control.
-svn add --force $dir_working --no-ignore --no-auto-props 
 
-# Find out what files are in the working copy, but not in
-# the original directory, and should thus be removed.
+echo "Adding any vendor drop items to version control..."
+svn add --force $dir_working --no-ignore --no-auto-props 
+echo "done."
+
+
+echo "Creating list of files to delete from working copy (because they no longer exist in the original source package)..."
+
 diff -r $dir_orig $dir_working | grep "Only in $dir_working" | sed -r -e 's/^.*Only in //' -e 's@: @/@' > ~/files_to_svn_delete.txt
 
 # Remove line with $dir_working/.svn from ~/files_to_svn_delete.txt, because we don't have to svn delete that one.
 sed -i "/$dir_working\/.svn/d" ~/files_to_svn_delete.txt
+
+echo "done."
+
 
 # Svn delete all files that are in the working copy, but not
 # in the directory with the original source.
@@ -40,10 +47,12 @@ sed -i "/$dir_working\/.svn/d" ~/files_to_svn_delete.txt
 #     See also http://svnbook.red-bean.com/en/1.7/svn.advanced.pegrevs.html
 #     Maybe we can even append an @ to every file???
 #   - files in the root dir are not in files_to_svn_delete.txt???
+echo "SVN deleting files from working copy that no longer exist in the original source package..."
 for file in `cat ~/files_to_svn_delete.txt`
 do
     svn delete "$file"
 done
+echo "done."
 
 # Are other modifications necessary?  Maybe case-sensitive related changes?
 # The files that changed their capitalization, probably show up as missing.  I
