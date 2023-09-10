0995d5f8c0de4b209c336f5fe1d4bc5e83caf31f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2023-09-11 00:51:12 +0200

    Fix ShellCheck warnings.

--- Bash/scripts/whodididelete.sh
+++ Bash/scripts/whodididelete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-grep "Deleted.*`date +"%a, %d %b %Y"`" $HOME/.mailfilter.log \
+grep "Deleted.*$(date +"%a, %d %b %Y")" "$HOME"/.mailfilter.log \
 | sed 's/mailfilter: Deleted \(.*\) <\([^>]*\)>:\(.*\)\[Applied filter.*/\1 <\2>\n\3\n/g'
 
-grep "Deleted.*`date +"%a, %d %b %Y"`" $HOME/.mailfilter.log \
+grep "Deleted.*$(date +"%a, %d %b %Y")" "$HOME"/.mailfilter.log \
 | sed 's/mailfilter: Deleted \(.*\) <\([^>]*\)>:\(.*\)\[Applied filter.*/\1 <\2>\n\3\n/g' | wc -l
 
 #| sed -e 's/mailfilter: Deleted.*<.*>.*/\1<>/g'
