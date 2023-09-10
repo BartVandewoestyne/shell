93e82f7080bb9957bcd96105655082dfba477ec1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2023-09-10 17:22:45 +0200

    Fix ShellCheck warnings.

--- Bash/scripts/path_operations.sh
+++ Bash/scripts/path_operations.sh
@@ -1,17 +1,19 @@
+#!/usr/bin/env bash
+
 # See http://stackoverflow.com/questions/370047/what-is-the-most-elegant-way-to-remove-a-path-from-the-path-variable-in-bash
 
 path_append () {
-  path_remove $1;
+  path_remove "$1";
   export PATH="$PATH:$1";
 }
 
 path_prepend () {
-  path_remove $1;
+  path_remove "$1";
   export PATH="$1:$PATH";
 }
 
 path_remove () {
-  export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`;
+  export PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//');
 }
 
 # Check whether directory exists and only add it if it is not in your path yet.
@@ -29,10 +31,10 @@ showpath() {
 
   # With indent.
   echo "PATH:"
-  echo $PATH | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
+  echo "$PATH" | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
   echo "LD_LIBRARY_PATH:"
-  echo $LD_LIBRARY_PATH | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
+  echo "$LD_LIBRARY_PATH" | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
   echo "MANPATH:"
-  echo $MANPATH | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
+  echo "$MANPATH" | awk -F: '{for(i=1;i<=NF;i++){printf("  %s\n",$i)}}'
 
 }
